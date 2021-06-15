b4ee82a736b437bcccf202e6e8d4ae0a549c4304
artemmarkaryan <artemmarkaryan@yandex.ru>
2021-06-15 22:58:04 +0300

    add gihub.io to allowed origins

--- cmd/app/internal/server/middleware/cors.go
+++ cmd/app/internal/server/middleware/cors.go
@@ -12,8 +12,21 @@ type CORSProvider struct{}
 func (C CORSProvider) GetMiddlewareFunc() gin.HandlerFunc {
 	log.Print("setting cors")
 	return cors.New(cors.Config{
-		AllowOrigins: []string{"http://localhost:5000", "http://localhost", "http://0.0.0.0", "http://127.0.0.1", "https://golangcrm.herokuapp.com"},
-		AllowMethods: []string{"PUT", "PATCH", "POST", "DELETE", "OPTIONS"},
+		AllowOrigins: []string{
+			"http://localhost:5000",
+			"https://kirillivano.github.io",
+			"http://localhost",
+			"http://0.0.0.0",
+			"http://127.0.0.1",
+			"https://golangcrm.herokuapp.com",
+		},
+		AllowMethods: []string{
+			"PUT",
+			"PATCH",
+			"POST",
+			"DELETE",
+			"OPTIONS",
+		},
 		AllowHeaders: []string{
 			"Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization",
 			"accept", "origin", "Cache-Control", "X-Requested-With"},
